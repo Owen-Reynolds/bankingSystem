2f53207bf259c4dac5640a85367f95da1020ad79
Owen-Reynolds <oreynold@ashland.edu>
2024-12-17 10:43:50 -0500

    Added Create Account Panel

--- bank.java
+++ bank.java
@@ -1,4 +1,7 @@
 public class bank {
     //Handles user authentication, account creation, and manages user accounts.
     //Methods: CreateUser(), AuthenticateUser(), ListAccounts(), FindAccountById().
+    public void CreateUser(){
+        
+    }
 }
--- main.java
+++ main.java
@@ -14,15 +14,17 @@ public class main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
         frame.setLocationRelativeTo(null);
-        
+
         cardLayout = new CardLayout();
         cardsPanel = new JPanel(cardLayout);
 
         JPanel loginPanel = createLoginPanel();
+        JPanel newUserPanel = createNewUserPanel();
         JPanel mainMenuPanel = createMainMenuPanel();
         JPanel transactions = createTransactionsPanel();
 
         cardsPanel.add(loginPanel, "Login Screen");
+        cardsPanel.add(newUserPanel, "New User Screen");
         cardsPanel.add(mainMenuPanel, "Main Menu Screen");
         cardsPanel.add(transactions, "Transaction Screen");
 
@@ -41,6 +43,8 @@ public class main {
         JPasswordField passwordField = new JPasswordField(20);
         JButton loginButton = new JButton("Login");
 
+        JButton createButton = new JButton("Create Account");
+
         loginButton.addActionListener(e ->{
             String username = usernameField.getText();
             String password = new String(passwordField.getPassword());
@@ -50,14 +54,58 @@ public class main {
                 JOptionPane.showMessageDialog(null, "Incorrect Login Information");
             }
         });
+        
+        createButton.addActionListener(e ->{
+            cardLayout.show(cardsPanel, "New User Screen");
+        });
 
         loginPanel.add(usernameLabel);
         loginPanel.add(usernameField);
         loginPanel.add(passwordLabel);
         loginPanel.add(passwordField);
         loginPanel.add(loginButton);
+        loginPanel.add(createButton);
 
         return loginPanel;
+    }
+
+    private static JPanel createNewUserPanel(){
+        JPanel newUserPanel = new JPanel();
+        newUserPanel.setLayout(new BoxLayout(newUserPanel, BoxLayout.Y_AXIS));
+
+        JLabel userTitle = new JLabel("Create Account");
+        JLabel newUserLabel = new JLabel("Create Username: ");
+        JLabel newPasswordLabel = new JLabel("Create Password: ");
+
+        JTextField newUserUsername = new JTextField(20);
+        JPasswordField newUserPassword = new JPasswordField(20);
+
+        JButton newUserButton = new JButton("Create Account");
+
+        newUserButton.addActionListener(e ->{
+            //Add user info to db
+            String newUsername = newUserUsername.getText();
+            String newPassword = new String(newUserPassword.getPassword());
+
+            if(newPassword.length() < 8){
+                JOptionPane.showMessageDialog(null, "Password must be at least 8 Characters");
+                cardLayout.show(cardsPanel, "New User Screen");
+            }
+            else{
+            JOptionPane.showMessageDialog(null, "Account Created!");
+            
+            cardLayout.show(cardsPanel, "Main Menu Screen");
+            }
+        });
+
+        newUserPanel.add(userTitle);
+        newUserPanel.add(newUserLabel);
+        newUserPanel.add(newUserUsername);
+        newUserPanel.add(newPasswordLabel);
+        newUserPanel.add(newUserPassword);
+        newUserPanel.add(newUserButton);
+
+        return newUserPanel;
     }
 
     private static JPanel createMainMenuPanel(){
